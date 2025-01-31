f18464a531b002a49b85ddf24d22665ff78f7b7b
William Mullen <wmulen@gmail.com>
2025-01-31 14:36:05 -0500

    0.2.12: Bugfix

--- module/actor-sheet.mjs
+++ module/actor-sheet.mjs
@@ -32,7 +32,7 @@
     // context.flags = actorData.flags;
 
     context.items = {
-      field: this.document.system.schema.getField("items"),
+      field: this.document.system.getField("items"),
       enriched: await TextEditor.enrichHTML(this.document.system.items, {
         relativeTo: this.document,
       }),
