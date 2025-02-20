862246e4d11ac0b5adc060f1faee567e20bdbd65
William Mullen <wmulen@gmail.com>
2025-02-20 01:07:11 -0500

    0.5.35 Testing

--- module/actor-sheet.mjs
+++ module/actor-sheet.mjs
@@ -124,6 +124,7 @@ export class OdysseyActorSheet extends api.HandlebarsApplicationMixin(
             }
           );
           let enrichAbilityListName = "enrichedAbilityList" + i;
+          console.log("NAME: " + enrichAbilityListName)
           context[enrichAbilityListName] = await TextEditor.enrichHTML(
             this.actor.system.themebooks[enrichThemebookName].abilityList,
             {
