863307f2ea5dcffd5e09704550b4052c8790b062
William Mullen <wmulen@gmail.com>
2025-02-19 23:17:18 -0500

    0.5.20 Typo fix

--- module/actor-sheet.mjs
+++ module/actor-sheet.mjs
@@ -110,7 +110,7 @@ export class OdysseyActorSheet extends api.HandlebarsApplicationMixin(
     switch (partId) {
       case 'themebooks':
         console.log("NUM OF THEMEBOOKS");
-        console.log(this.actor.themebooks.length);
+        console.log(this.actor.system.themebooks.length);
         for (var i = 0; i <= this.actor.system.themebooks.length; i++) {
           console.log("JS BASIC MOVE");
           console.log("enrichedBasicMove" + i);
